20528e1913d95cc05c9624892551640c771ceb9e
Rizky Darmawan <darmawanrizky43@gmail.com>
2026-01-30 11:20:02 +0700

    ref: remove unused code

--- internal/applications/auth/service/auth_service.go
+++ internal/applications/auth/service/auth_service.go
@@ -10,8 +10,4 @@ import (
 type AuthService interface {
 	Register(ctx context.Context, param *dto.ParameterRegister) (*models.User, error)
 	Login(ctx context.Context, req *dto.LoginRequest, ipAddress, userAgent string) (*dto.LoginResponse, error)
-	// RefreshToken(ctx context.Context, req *dto.RefreshTokenRequest) (*dto.RefreshTokenResponse, error)
-	// Logout(ctx context.Context, req *dto.LogoutRequest, userID int64) error
-	// LogoutAll(ctx context.Context, userID int64) (*dto.LogoutAllResponse, error)
-	// GetMe(ctx context.Context, userID int64) (*dto.GetMeResponse, error)
 }
