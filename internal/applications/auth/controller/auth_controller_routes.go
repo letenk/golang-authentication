b4d119dc883400a7d1f031601f403404947fc83f
Rizky Darmawan <darmawanrizky43@gmail.com>
2026-01-30 11:21:42 +0700

    ref: remove unused route code

--- internal/applications/auth/controller/auth_controller_routes.go
+++ internal/applications/auth/controller/auth_controller_routes.go
@@ -8,13 +8,6 @@ import (
 func (controller *AuthController) AddRoutes(e *echo.Echo, authMiddleware middleware.AuthenticationMiddleware) {
 	groupV1 := e.Group("/api/v1/auth")
 
-	// Public routes (no authentication required)
 	groupV1.POST("/register", controller.Register)
 	groupV1.POST("/login", controller.Login)
-	// groupV1.POST("/refresh-token", controller.RefreshToken)
-
-	// Protected routes (authentication required)
-	// groupV1.POST("/logout", controller.Logout, authMiddleware.Authenticate(true))
-	// groupV1.POST("/logout-all", controller.LogoutAll, authMiddleware.Authenticate(true))
-	// groupV1.GET("/me", controller.GetMe, authMiddleware.Authenticate(true))
 }
